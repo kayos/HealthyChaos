5662bd897bbcd8d6344140cca94bc9c10e4bed4d
kayos <4426302+kayos@users.noreply.github.com>
2025-05-07 16:42:12 +0100

    Split out Top bar into separate DeviceBar composable

---
+++ app/src/main/java/com/kayos/healthykayos/DeviceBar.kt
@@ -0,0 +1,33 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package com.kayos.healthykayos
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+internal fun DeviceBar(){
+    TopAppBar(
+        title = { Text("No device") },
+        actions = {
+            IconButton(onClick = { /* TODO: go to connection screen */ }) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = "Connect Device")
+            }
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = Color.DarkGray,
+            titleContentColor = Color.White,
+            actionIconContentColor = Color.White
+        )
+    )
+}
--- app/src/main/java/com/kayos/healthykayos/MainScreen.kt
+++ app/src/main/java/com/kayos/healthykayos/MainScreen.kt
@@ -1,16 +1,8 @@
 package com.kayos.healthykayos
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -18,21 +10,7 @@ import androidx.compose.ui.tooling.preview.Preview
 @Preview
 fun MainScreen() {
     Scaffold(
-        topBar = {
-            TopAppBar(
-                title = { Text("No device") },
-                actions = {
-                    IconButton(onClick = { /* TODO: go to connection screen */ }) {
-                        Icon(imageVector = Icons.Default.Add, contentDescription = "Connect Device")
-                    }
-                },
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = Color.DarkGray,
-                    titleContentColor = Color.White,
-                    actionIconContentColor = Color.White
-                )
-            )
-        },
+        topBar = {DeviceBar()},
         content = { _ -> HomeScreen() }
     )
 }
