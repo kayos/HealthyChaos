349e2a52335315fcd8d5a368cdb9f5b94db068f2
kayos <4426302+kayos@users.noreply.github.com>
2025-05-08 01:39:34 +0100

    Use the PolarAPI factory in HeartRateSensor

--- data/polar/src/main/java/com/kayos/polar/PolarHeartRateSensor.kt
+++ data/polar/src/main/java/com/kayos/polar/PolarHeartRateSensor.kt
@@ -42,18 +42,7 @@ internal class PolarHeartRateSensor private constructor(context: Context) : IHea
     val hrSampleRateSec = 1
 
     //TODO make private once refactor is done
-    val api: PolarBleApi = PolarBleApiDefaultImpl.defaultImplementation(
-        context,
-        setOf(
-            PolarBleApi.PolarBleSdkFeature.FEATURE_HR,
-            PolarBleApi.PolarBleSdkFeature.FEATURE_POLAR_SDK_MODE,
-            PolarBleApi.PolarBleSdkFeature.FEATURE_BATTERY_INFO,
-            PolarBleApi.PolarBleSdkFeature.FEATURE_POLAR_OFFLINE_RECORDING,
-            PolarBleApi.PolarBleSdkFeature.FEATURE_POLAR_ONLINE_STREAMING,
-            PolarBleApi.PolarBleSdkFeature.FEATURE_DEVICE_INFO,
-            PolarBleApi.PolarBleSdkFeature.FEATURE_POLAR_LED_ANIMATION
-        )
-    )
+    val api: PolarBleApi = PolarApiFactory.getPolarApi(context)
 
     companion object {
         private const val TAG = "PolarHeartRateSensor"
