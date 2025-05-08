887a13149c9d788ac5a27c8a8d9a6f0527c45c26
kayos <4426302+kayos@users.noreply.github.com>
2025-05-08 01:17:21 +0100

    Move Connection Composable into its own file as ConnectionScreen

--- app/src/main/java/com/kayos/healthykayos/ConnectionFragment.kt
+++ app/src/main/java/com/kayos/healthykayos/ConnectionFragment.kt
@@ -4,33 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
-import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.fragment.findNavController
-import com.kayos.polar.Device
 import com.kayos.polar.HeartRateProviderFactory
 import com.kayos.polar.IHeartRateSensor
 
@@ -51,7 +33,7 @@ class ConnectionFragment : Fragment() {
             setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
             setContent {
                 MaterialTheme {
-                    Connections(
+                    ConnectionScreen(
                         sensor,
                         onRecordingsClick = {
                             findNavController().navigate(R.id.action_ConnectionFragment_to_RecordingsFragment)
@@ -70,91 +52,11 @@ class ConnectionFragment : Fragment() {
 }
 
 @Composable
-fun Connections(sensor: IHeartRateSensor,
-                onRecordingsClick: () -> Unit,
-                onLiveClick: () -> Unit,
-                viewModel: ConnectionViewModel = viewModel(factory = ConnectionViewModel.Factory)){
+fun ConnectionScreen(sensor: IHeartRateSensor,
+                     onRecordingsClick: () -> Unit,
+                     onLiveClick: () -> Unit,
+                     viewModel: ConnectionViewModel = viewModel(factory = ConnectionViewModel.Factory)){
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
 
-    Connections(sensor, onRecordingsClick, onLiveClick, uiState)
-}
-
-@Composable
-fun Connections(
-    sensor: IHeartRateSensor,
-    onRecordingsClick: () -> Unit,
-    onLiveClick: () -> Unit,
-    uiState: ConnectionUiState
-)
-{
-    val connectedDevice = sensor.connectedDevices.collectAsState().value
-
-    Column {
-        Button(onClick = {
-            sensor.search()
-        }) {
-            Text("Scan Devices")
-        }
-        Column {
-            uiState.availableDevices.forEach { device ->
-                DeviceItem(device, onClick = { sensor.connect(device.id) })
-            }
-        }
-        Spacer(modifier = Modifier.height(16.dp))
-
-        if (connectedDevice != null)
-            Device(Device(connectedDevice.deviceId, connectedDevice.name), onRecordingsClick, onLiveClick)
-    }
-}
-
-@Composable
-fun Device(device: Device, onRecordingsClick: () -> Unit, onLiveClick: () -> Unit) {
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .padding(16.dp)
-        .background(Color.LightGray)) {
-
-        Text("Sensor: ${device.id}", style = MaterialTheme.typography.titleLarge)
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Column {
-            Button(
-                onClick = onRecordingsClick,
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Text("Recordings")
-            }
-            Button(
-                onClick = onLiveClick,
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Text("Live HR")
-            }
-        }
-
-    }
-}
-
-@Composable
-fun DeviceItem(device: Device, onClick: () -> Unit) {
-    Card(
-        modifier = Modifier
-            .padding(8.dp)
-            .clickable(onClick = onClick),
-        elevation = CardDefaults.cardElevation(4.dp),
-        colors = CardDefaults.cardColors(
-            containerColor = MaterialTheme.colorScheme.primaryContainer
-        )
-    ) {
-        ListItem(
-            modifier = Modifier.padding(16.dp),
-            headlineContent = {
-                Text(text = device.name, style = MaterialTheme.typography.bodyLarge)
-            },
-            supportingContent = {
-                Text(text = "Id: ${device.id}", style = MaterialTheme.typography.bodyMedium)
-            }
-        )
-    }
+    ConnectionScreen(sensor, onRecordingsClick, onLiveClick, uiState)
 }
---
+++ app/src/main/java/com/kayos/healthykayos/ConnectionScreen.kt
@@ -0,0 +1,104 @@
+package com.kayos.healthykayos
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.kayos.polar.Device
+import com.kayos.polar.IHeartRateSensor
+
+
+@Composable
+fun ConnectionScreen(
+    sensor: IHeartRateSensor,
+    onRecordingsClick: () -> Unit,
+    onLiveClick: () -> Unit,
+    uiState: ConnectionUiState
+)
+{
+    val connectedDevice = sensor.connectedDevices.collectAsState().value
+
+    Column {
+        Button(onClick = {
+            sensor.search()
+        }) {
+            Text("Scan Devices")
+        }
+        Column {
+            uiState.availableDevices.forEach { device ->
+                DeviceItem(device, onClick = { sensor.connect(device.id) })
+            }
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+
+        if (connectedDevice != null)
+            Device(Device(connectedDevice.deviceId, connectedDevice.name), onRecordingsClick, onLiveClick)
+    }
+}
+
+@Composable
+fun Device(device: Device, onRecordingsClick: () -> Unit, onLiveClick: () -> Unit) {
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(16.dp)
+        .background(Color.LightGray)) {
+
+        Text("Sensor: ${device.id}", style = MaterialTheme.typography.titleLarge)
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Column {
+            Button(
+                onClick = onRecordingsClick,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("Recordings")
+            }
+            Button(
+                onClick = onLiveClick,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("Live HR")
+            }
+        }
+
+    }
+}
+
+@Composable
+fun DeviceItem(device: Device, onClick: () -> Unit) {
+    Card(
+        modifier = Modifier
+            .padding(8.dp)
+            .clickable(onClick = onClick),
+        elevation = CardDefaults.cardElevation(4.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer
+        )
+    ) {
+        ListItem(
+            modifier = Modifier.padding(16.dp),
+            headlineContent = {
+                Text(text = device.name, style = MaterialTheme.typography.bodyLarge)
+            },
+            supportingContent = {
+                Text(text = "Id: ${device.id}", style = MaterialTheme.typography.bodyMedium)
+            }
+        )
+    }
+}
