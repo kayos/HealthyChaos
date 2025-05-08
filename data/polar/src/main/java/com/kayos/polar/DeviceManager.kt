b6b01930973e56cae2936371c55148f9343c1983
kayos <4426302+kayos@users.noreply.github.com>
2025-05-08 02:01:20 +0100

    Add (dis)connect to DeviceManager

--- data/polar/src/main/java/com/kayos/polar/DeviceManager.kt
+++ data/polar/src/main/java/com/kayos/polar/DeviceManager.kt
@@ -1,8 +1,17 @@
 package com.kayos.polar
 
+import android.util.Log
+import com.polar.androidcommunications.api.ble.model.DisInfo
 import com.polar.sdk.api.PolarBleApi
+import com.polar.sdk.api.PolarBleApiCallback
+import com.polar.sdk.api.model.PolarDeviceInfo
+import com.polar.sdk.api.model.PolarHealthThermometerData
+import com.polar.sdk.api.model.PolarHrData
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.reactive.asFlow
+
 
 class DeviceManager(private val api: PolarBleApi) {
 
@@ -18,6 +27,59 @@ class DeviceManager(private val api: PolarBleApi) {
             }
     }
 
+    val connectedDevice : Flow<Device?> = callbackFlow {
+        val callback = object : PolarBleApiCallback() {
+            override fun deviceConnected(polarDeviceInfo: PolarDeviceInfo) {
+                Log.d(TAG, "CONNECTED: ${polarDeviceInfo.deviceId}")
+                trySend(Device(polarDeviceInfo.deviceId, polarDeviceInfo.name))
+            }
+
+            override fun deviceConnecting(polarDeviceInfo: PolarDeviceInfo) {
+                Log.d(TAG, "CONNECTING: ${polarDeviceInfo.deviceId}")
+            }
+
+            override fun deviceDisconnected(polarDeviceInfo: PolarDeviceInfo) {
+                Log.d(TAG, "DISCONNECTED: ${polarDeviceInfo.deviceId}")
+                trySend(null)
+            }
+
+            override fun batteryLevelReceived(identifier: String, level: Int) {
+                Log.d(TAG, "BATTERY LEVEL: $level")
+            }
+
+            override fun blePowerStateChanged(powered: Boolean) {
+                //don't care
+            }
+
+            override fun disInformationReceived(
+                identifier: String,
+                disInfo: DisInfo
+            ) {
+                // don't care
+            }
+
+            override fun hrNotificationReceived(
+                identifier: String,
+                data: PolarHrData.PolarHrSample
+            ) {
+                // deprecated
+            }
+
+            override fun htsNotificationReceived(
+                identifier: String,
+                data: PolarHealthThermometerData
+            ) {
+                // don't care
+            }
+
+        }
+        api.setApiCallback(callback)
+
+        awaitClose {
+           //TODO figure out to unregister from api
+        }
+    }
+
     fun search(): Flow<List<Device>> {
         api.setPolarFilter(true)
 
@@ -26,6 +88,14 @@ class DeviceManager(private val api: PolarBleApi) {
             devices.add(Device(entry.deviceId, entry.name))
             devices.toList()
         }.asFlow()
+    }
+
+    fun connect(deviceId: String) {
+        api.connectToDevice(deviceId)
+    }
+
+    fun disconnect(deviceId: String) {
+        api.disconnectFromDevice(deviceId)
     }
 }
 
