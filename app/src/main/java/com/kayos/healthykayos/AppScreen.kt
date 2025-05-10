1909125a68c61383f718a97828544b945a0ae08a
kayos <4426302+kayos@users.noreply.github.com>
2025-05-10 21:47:33 +0100

    Remember navController

--- app/src/main/java/com/kayos/healthykayos/AppScreen.kt
+++ app/src/main/java/com/kayos/healthykayos/AppScreen.kt
@@ -4,11 +4,14 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 @Preview
 fun AppScreen() {
+    val navController = rememberNavController()
+
     Scaffold(
         topBar = {DeviceBar()},
         content = { _ -> HomeScreen() }
