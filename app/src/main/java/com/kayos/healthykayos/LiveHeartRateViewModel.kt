1b8309463752b823cb98f42938deaafeca877330
kayos <4426302+kayos@users.noreply.github.com>
2025-05-28 19:39:16 +0100

    Use streaming capable device instead of HeartRate Sensor

--- app/src/main/java/com/kayos/healthykayos/LiveHeartRateViewModel.kt
+++ app/src/main/java/com/kayos/healthykayos/LiveHeartRateViewModel.kt
@@ -2,24 +2,67 @@ package com.kayos.healthykayos
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.kayos.polar.DeviceManager
+import com.kayos.polar.HeartRate
 import com.kayos.polar.HeartRateProviderFactory
 import com.kayos.polar.IHeartRateSensor
+import com.kayos.polar.IStreamAPI
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
-class LiveHeartRateViewModel(val sensor: IHeartRateSensor) : ViewModel()
+class LiveHeartRateViewModel(val sensor: IHeartRateSensor,
+                             deviceManager: DeviceManager = DeviceManager.getInstance()) : ViewModel()
 {
-    val heartRate = sensor.heartRate
+    private val _shouldStream = MutableStateFlow(false)
+    private val _streamingDevice : StateFlow<IStreamAPI?> =
+        deviceManager.connectedDevice.map {
+                device -> device?.getStreamFunctionality()}
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(),
+                initialValue = null,
+            )
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    val heartRate: StateFlow<HeartRate?> = combine(
+        _shouldStream,
+        _streamingDevice.filterNotNull()
+    ){ streaming, device -> Pair(streaming,device) }
+    .flatMapLatest{ (streaming, device) ->
+        if (streaming) device.startStream()
+        else { flowOf(null) }
+    }
+    .stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(),
+        initialValue = null,
+    )
+
+   // val heartRate = sensor.heartRate
 
     fun startStreaming() {
         sensor.startHeartRateStream()
+        _shouldStream.value = true
     }
 
     fun stopStreaming() {
         sensor.stopHeartRateStream()
+        _shouldStream.value = false
     }
 
     companion object {
+        private const val TAG = "LiveHearRateViewModel"
         val Factory: ViewModelProvider.Factory = viewModelFactory {
             initializer {
                 val application = checkNotNull(this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY])
---
+++ app/src/test/java/com/kayos/healthykayos/LiveHeartRateViewModelTest.kt
@@ -0,0 +1,72 @@
+package com.kayos.healthykayos
+
+import app.cash.turbine.test
+import com.kayos.healthykayos.testutils.MainDispatcherRule
+import com.kayos.polar.Device
+import com.kayos.polar.DeviceManager
+import com.kayos.polar.HeartRate
+import com.kayos.polar.IHeartRateSensor
+import com.kayos.polar.IStreamAPI
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.mock
+import org.robolectric.RobolectricTestRunner
+import java.time.Instant
+import kotlin.test.assertNull
+
+@OptIn(ExperimentalCoroutinesApi::class)
+@RunWith(RobolectricTestRunner::class)
+class LiveHeartRateViewModelTest {
+
+    @get:Rule
+    val mainDispatcherRule = MainDispatcherRule()
+
+    @Test
+    fun startStreaming_emitsHeartRate() = runTest {
+        val expected = HeartRate(Instant.now(), 123)
+        val streamApi = mock<IStreamAPI> {
+            on { startStream() } doReturn flowOf(expected)
+        }
+        val device = mock<Device>{
+            on { getStreamFunctionality() } doReturn streamApi
+        }
+        val deviceManager = DeviceManager()
+        deviceManager.notifyDeviceConnected(device)
+        val viewModel = LiveHeartRateViewModel(mock<IHeartRateSensor>{},deviceManager)
+
+        viewModel.heartRate.test{
+            awaitItem()
+            viewModel.startStreaming()
+
+            assertEquals(expected, awaitItem())
+        }
+    }
+
+    @Test
+    fun stopStreaming_stopsHeartRateEmission() = runTest {
+        val expected = HeartRate(Instant.now(), 123)
+        val streamApi = mock<IStreamAPI> {
+            on { startStream() } doReturn flowOf(expected)
+        }
+        val device = mock<Device>{
+            on { getStreamFunctionality() } doReturn streamApi
+        }
+        val deviceManager = DeviceManager()
+        deviceManager.notifyDeviceConnected(device)
+        val viewModel = LiveHeartRateViewModel(mock<IHeartRateSensor>{},deviceManager)
+
+        viewModel.heartRate.test{
+            awaitItem()
+            viewModel.startStreaming()
+            assertEquals(expected, awaitItem())
+            viewModel.stopStreaming()
+            assertNull(awaitItem())
+        }
+    }
+}
