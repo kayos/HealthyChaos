ea940319976897883cae64bb2ef4c946d3a2662e
kayos <4426302+kayos@users.noreply.github.com>
2025-05-29 00:01:37 +0100

    Introduce a more generic data recording type

---
+++ data/device/src/main/java/com/kayos/device/HeartRateRecordingData.kt
@@ -0,0 +1,17 @@
+package com.kayos.device
+
+import java.util.Calendar
+
+sealed class RecordingData(
+    val startTime: Calendar){
+
+    class HeartRateRecording(startTime: Calendar, val data: HrData) :
+        RecordingData(startTime)
+}
+
+data class HrData(val samples: List<HrSample>) {
+    data class HrSample(
+        val bpm: Int,
+        val secondsFromStart: Long
+    )
+}
